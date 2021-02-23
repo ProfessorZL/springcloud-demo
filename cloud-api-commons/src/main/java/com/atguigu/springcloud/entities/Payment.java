7868e5794933e1fbd6009b70626615b79905c115
prozl <453454120@qq.com>
2021-02-24 00:41:24 +0800

    冲突解决提交

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -13,11 +13,5 @@ public class Payment implements Serializable {
 
     private Long id;
     private String serial;
-    private String name;
-
-    public static void main(String[] args) {
-        System.out.println("测试git分支");
-        System.out.println("master分支啊");
-    }
 
 }
