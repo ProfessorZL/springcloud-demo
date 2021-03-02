bf802ba59dfb075ac0e3dd687e08123533ea9e19
prozl <453454120@qq.com>
2021-03-03 00:49:43 +0800

    dashboard监控

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentMainHytx8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentMainHytx8001.java
@@ -1,9 +1,12 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,21 @@ public class PaymentMainHytx8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentMainHytx8001.class,args);
+    }
+
+    /**
+     * 此配置为服务监控而配置，与服务熔断无关，是Spring cloud升级够的坑.
+     * ServletRegistration因为springboot默认路径不是/hystrix.stream
+     * 只需要在自己项目里配置下面servlet就可以了
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
